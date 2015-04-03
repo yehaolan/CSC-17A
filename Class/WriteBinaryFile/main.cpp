504d46be5274c79cdc0999d905f08b77e5b3d643
benjaminyhl <yehaolan@gmail.com>
2015-04-03 09:11:27 -0700

    yehaolan@gmail.com

--- Class/WriteBinaryFile/main.cpp
+++ Class/WriteBinaryFile/main.cpp
@@ -26,98 +26,105 @@ Student *readStu(char *,int);
 
 //Execution begins here
 int main(int argc, char** argv) {
-    //set seed for random number 
+    //Initialize random number generator
     srand(static_cast<unsigned int>(time(0)));
-    //fill the Student array
-    int nStu=36;
-    Student *stu=filStu(nStu);
-    //write to a file all student in binary form
-    char fn[]="data.dat";
-    wrtStu(fn,stu,nStu);
-    //print out student record
+    //Fill the Student array
+    int nStudents=36;
+    Student *stu=filStu(nStudents);
+    //Write to a file all the students in binary form
+    char fn[]="stu.dat";
+    cout<<endl<<"Generate the data file"<<endl;
+    wrtStu(fn,stu,nStudents);
+    //Print out the student records
     cout<<"Size of a student record = "<<sizeof(Student)<<endl;
-    cout<<"Size of all student record = "<<sizeof(stu)<<endl;
-    prntStu(stu,nStu);
-    //randomly choose a student
-    int rStudent=rand()%nStu+1;
-    //print the random student
-    cout<<endl<<"Read back the "<<rStudent<<" st/nd/rd/th"<<endl;
-    //find the 35th student
-    Student *one=readStu(fn,35);
-    //print 35th student
-    prntStu(one,1);
-    wrtStu1(fn,one,nStu);
-    //deallocate memory
-    delete [] stu;
+    cout<<"Size of all student records = "<<sizeof(stu)<<endl;
+    prntStu(stu,nStudents);
+    //Randomly chose a student
+    int rStudent=rand()%nStudents+1;
+    cout<<endl<<"Read back the "<<rStudent<<" st/nd/rd/th student in the file"<<endl;
+    Student *one=readStu(fn,rStudent);
+    //Print the random student
+    prntStu1(one,rStudent);
+    //Write this to a random position
+    //rStudent=rand()%nStudents+1;
+    //cout<<"The random position written = "<<rStudent<<endl;
+    //wrtStu1(fn,one,rStudent);
+    //Deallocate memory
+    delete []stu;
     delete one;
-    
-    stu=one=0;
     //Exit stage right
     return 0;
 }
 
-Student *readStu(char *fn,int rec) {
-    //open file for binary
+Student *readStu(char *fn,int rec){
+    //Open file for binary
     fstream in;
-    in.open(fn,ios::in|ios::out|ios::binary);
-    //allocate memory for 1 student
+    in.open(fn,ios::out|ios::in|ios::binary);
+    //Allocate memory for 1 student;
     Student *one=new Student;
-    cout<<"Where pointer is "<<one<<endl;
+    //Position read pointer;
     long position=(rec-1)*sizeof(Student);
-    //position pointer
     in.seekg(position,ios::beg);
-    //read to file
-    in.read(reinterpret_cast<char *>(one),sizeof(Student)*rec);
-    //close the file
+    //Read from the file
+    in.read(reinterpret_cast<char *>(one),sizeof(Student));
+    //Close and exit
     in.close();
+    //Return the record
     return one;
 }
 
-void wrtStu(char *fn,Student *a,int n) {
-    //open file for binary
+void wrtStu1(char *fn,Student *a,int n){
+    //Open file for binary
     fstream out;
     out.open(fn,ios::out|ios::binary);
-    //write to file
+    //Position read pointer;
+    long position=(n-1)*sizeof(Student);
+    out.seekp(position,ios::beg);
+    //Write to the file
+    out.write(reinterpret_cast<char *>(a),sizeof(Student));
+    //Close and exit
+    out.close();
+}
+
+void wrtStu(char *fn,Student *a,int n){
+    //Open file for binary
+    fstream out;
+    out.open(fn,ios::out|ios::binary);
+    //Write to the file
     out.write(reinterpret_cast<char *>(a),sizeof(Student)*n);
-    //close the file
+    //Close and exit
     out.close();
 }
 
-void wrtStu1(char *fn,Student *a,int n) {
-    //open file for binary
-    fstream out;
-    out.open(fn,ios::out|ios::binary);
-    //write to file
-    out.write(reinterpret_cast<char *>(a[n-1]),sizeof(Student));
-    //close the file
-    out.close();
-}
-
-void prntStu(Student *a,int n) {
-    //open file for binary
-    fstream out;
-    
-    //output results
+void prntStu(Student *a,int n){
+    //Output the results
     for(int rec=0;rec<n;rec++){
-        cout<<"Student "<<(rec+1)<<"  ";
-        cout<<"Student ID = "<<a[rec].sid<<" ";
-        cout<<"Score = "<<a[rec].score<<" ";
-        cout<<"Grade = "<<a[rec].grade<<endl;
+        cout<<"Student "<<rec+1<<" ";
+        cout<<"Student id = "<<a[rec].sid;
+        cout<<" Score = "<<a[rec].score;
+        cout<<" Grade = "<<a[rec].grade<<endl;
     }
 }
 
-Student *filStu(int n) {
-    //allocate memory
+void prntStu1(Student *a,int n){
+    //Output the results
+    cout<<"Student "<<n<<" ";
+    cout<<"Student id = "<<a->sid;
+    cout<<" Score = "<<a->score;
+    cout<<" Grade = "<<a->grade<<endl;
+}
+
+Student *filStu(int n){
+    //Allocate memory
     Student *csc17a=new Student[n];
-    //fill each record
+    //Fill each record
     for(int rec=0;rec<n;rec++){
         csc17a[rec].sid=rand();
         csc17a[rec].score=rand()%30+70;
-        if(csc17a[rec].score>89) csc17a[rec].grade='A';
-        else if(csc17a[rec].score>79) csc17a[rec].grade='B';
+        if(csc17a[rec].score>89)csc17a[rec].grade='A';
+        else if(csc17a[rec].score>79)csc17a[rec].grade='B';
         else csc17a[rec].grade='C';
     }
-    //return the records
+    //Return the records
     return csc17a;
 }
-
--- Hmwk/Assignment3/Gaddis_8thEd_Chap12_ProgChal11/main.cpp
+++ Hmwk/Assignment3/Gaddis_8thEd_Chap12_ProgChal11/main.cpp
@@ -9,19 +9,62 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <string>
 using namespace std;
 
 //User Libraries
+#include "Sales.h"
 
 //global Constants
 
 //Function Prototypes
-
+Sales *filDiv(int);
 
 //Execution begins here
 int main(int argc, char** argv) {
-    
+    fstream out;
+    out.open("Data.dat",ios::out|ios::binary);
+    //allocate memory
+    Sales *div=filDiv(16);
+    //prompt user for each quarter sales
+    for(int i=0;i<16;i++) {
+        do {
+            cout<<div[i].name<<"(quarter "<<div[i].quarter<<"): ";
+            cin>>div[i].qrtSale;
+            if(div[i].qrtSale<0)
+                cout<<"Invalid input"<<endl;
+        } while(div[i].qrtSale<0);
+    }
+    //write into file
+    if(!out.fail()) {
+        out.write()
+    }
+    for(int i=0;i<16;i++) {
+        cout<<div[i].name<<" ";
+        if(i%4==3) cout<<endl;
+    }
+    out.close;
+    //deallocate memory
+    delete [] div;
+    div=0;
     //Exit stage right
     return 0;
 }
 
+//fill the name and quarter automatically
+Sales *filDiv(int n) {
+    Sales *div=new Sales[n];
+    //element 0-3 is South, 4-7 West, 8-11 North, 12-15 South
+    for(int i=0;i<n;i++) {
+        if(i<4) div[i].name="East";
+        else if(i<8) div[i].name="West";
+        else if(i<12) div[i].name="North";
+        else if(i<16) div[i].name="South";
+        if(i%4==0) div[i].quarter=1;
+        else if(i%4==1) div[i].quarter=2;
+        else if(i%4==2) div[i].quarter=3;
+        else if(i%4==3) div[i].quarter=4;
+        
+    }
+    return div;
+}
